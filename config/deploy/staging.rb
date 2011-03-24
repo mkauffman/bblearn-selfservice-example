ef1b89e92ceeb314c11d3d3b0017173ea6581620
Mark Bykerk Kauffman <mkauffman@csuchico.edu>
2011-03-24 13:31:49 -0700

    adding cap stuff

---
+++ config/deploy/staging.rb
@@ -0,0 +1,8 @@
+# =============================================================================
+# ROLES
+# =============================================================================
+role :web, "apphostdev.csuchico.edu"
+role :app, "apphostdev.csuchico.edu"
+role :db,  "apphostdev.csuchico.edu", :primary=>true
+set :branch, "staging"
+set :repository_cache, "git_staging"
