2166bb1d959d6ceccaf0ef75af1152b19d2ebbd8
Mark Bykerk Kauffman <mkauffman@csuchico.edu>
2011-03-24 13:33:21 -0700

    fixed some cap stuff

--- config/deploy/rfp.rb
+++ config/deploy/rfp.rb
@@ -6,4 +6,4 @@ role :app, "apphost.csuchico.edu"
 role :db,  "apphost.csuchico.edu", :primary=>true
 set :branch, "rfp"
 set :repository_cache, "git_rfp"
-set :deploy_to, "/h/dlt/opt/RailsApps/vista-rfp"
+set :deploy_to, "/h/dlt/opt/RailsApps/bblearn-rfp"
