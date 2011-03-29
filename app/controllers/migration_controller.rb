7561868f9d67b663abba1c421c542c502d30bea4
Michael Carlson <mcarlson13@mail.csuchico.edu>
2011-03-29 16:15:49 -0700

    testing savon gem.

--- app/controllers/migration_controller.rb
+++ app/controllers/migration_controller.rb
@@ -1,4 +1,8 @@
 class MigrationController < ApplicationController
+  require 'rubygems'
+  require 'savon'
+  require 'gyoku'
+  
   def index
     @sections = Section.find_sections_by_primary_instructor_id(session[:on_behalf_of])
     terms = Term.find_sso_terms
@@ -7,8 +11,24 @@ class MigrationController < ApplicationController
       @option_string += "<option>"+term.description+"</option>"
     end # for term
   end # index
-
+  
   def form
+    @client = Savon::Client.new do
+      wsdl.document = "https://lms-temp.csuchico.edu/webapps/ws/services/Context.WS?wsdl"
+      wsdl.endpoint = "https://lms-temp.csuchico.edu/webapps/ws/services/Context.WS"
+      wsdl.namespace = "http://context.ws.blackboard/" 
+    end
+    
+    @response = @client.request :initialize do  
+      @wsse.credentials "session", "nosession"
+      @wsse.timestamp = true
+      @soap.namespaces["xmlns:wsa"] = "http://www.w3.org/2005/08/addressing"
+      @soap.namespaces["xmlns:wsu"] = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+      @soap.header = { "wsa:To" => "https://lms-temp.csuchico.edu/webapps/ws/services/Context.WS", "wsa:MessageID" => "urn:uuid:182F29861349EF02E01251997573947", "wsa:Action" => "initialize" }
+    end
+    
+    #puts response.to_xml;
+    @newr = response.to_hash
+    #puts newr[:initialize_response][:return]
   end # form
-
 end # Migration Controller
---
+++ test/unit/bbl_section_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class BblSectionTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
