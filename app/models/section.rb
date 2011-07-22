7b0992a38fa4ce527748d5c1c6857b1885dfae75
Michael Wood <mykewould@gmail.com>
2011-07-21 17:13:27 -0700

    Jimmy again

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,20 +1,86 @@
 class Section < ActiveRecord::Base
-  def self.columns() @columns ||= []; end
+  attr_accessor :id, :source, :lc_id, :section_id, :short_name,
+   :long_name, :full_name, :timeframe_begin, :timeframe_restrict_begin,
+   :timeframe_end, :timeframe_restrict_end, :accept_enrollment, :datasource,
+   :parent_source, :parent_id, :template_parent_id, :template_parent_source,
+   :template_zip_or_epk_path, :template_none, :delivery_unit_type, :term_name,
+   :term_id, :admin_period, :term_datasource, :primary_instructor_name,
+   :primary_instructor_id, :admin_xlist_sect_type, :level, :created_at,
+   :updated_at, :student_list_file_name, :student_list_content_type,
+   :student_list_file_size, :student_list_updated_at, :status
 
-  def self.column(name, sql_type = nil, default = nil, null = true)
-    columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
+  def self.find_by_user(user_id)
+    sql = <<__SQL__
+    SELECT csm.COURSE_NAME
+    FROM dbTable.COURSE_MAIN csm
+    JOIN (SELECT csu.CRSMAIN_PK1
+          FROM dbTable.COURSE_USERS csu
+          JOIN (SELECT PK1
+                FROM dbTable.USERS
+                WHERE USER_ID = :user_id) usr
+          ON csu.USERS_PK1 = usr.PK1) crs
+    ON csm.PK1 = crs.CRSMAIN_PK1
+__SQL__
+
+
+    cursor = Section.build_cursor(sql)
+    cursor.bind_param(':user_id', user_id)
+
+    #logger.info ' *** The user \''+user_id+'\' is in the following courses: '+courses.join(",")
+    return Section.build_section(cursor)
   end
 
-  column :section_id, :string
 
+  def self.find_by_user_and_role(user_id,role_name)
+    sql = <<__SQL__
+    SELECT csm.COURSE_NAME
+    FROM dbTable.COURSE_MAIN csm
+    JOIN (SELECT csu.CRSMAIN_PK1
+          FROM dbTable.COURSE_USERS csu
+          JOIN (SELECT PK1
+                FROM dbTable.USERS
+                WHERE USER_ID = :user_id) usr
+          ON csu.USERS_PK1 = usr.PK1
+          WHERE csu.ROLE = :role_name) crs
+    ON csm.PK1 = crs.CRSMAIN_PK1
+__SQL__
 
-  def find_by_user
+    cursor = Section.build_cursor(sql)
+    cursor.bind_param(':user_id', user_id)
+    cursor.bind_param(':role_name', role_name)
 
+    #logger.info ' *** The user \''+user_id+'\' is in the following courses with role \''+role_name+'\': '+courses.join(",")
+    return Section.build_section(cursor)
   end
 
-  def find
 
+  def self.build_cursor(sql)
+    # Substitute values into query and execute:
+    $bbl_db_table = AppConfig.bbl_db_table
+    sql = sql.gsub!("dbTable",$bbl_db_table)
+    $bbl_db_conn = OCI8.new(AppConfig.bbl_db_user, AppConfig.bbl_db_password, AppConfig.bbl_db_string)
+    cursor = $bbl_db_conn.parse(sql)
   end
+
+  def self.build_section(cursor)
+    cursor.exec
+    sections = Array.new
+
+    while rs_row = cursor.fetch do
+      section       = Section.new
+      section.id   = rs_row[0]
+      sections.push(section)
+    end
+
+    if sections.size > 1
+      return sections
+    else
+      return section
+    end
+  end
+
+
+
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,72 +1,35 @@
-require 'active_record/validations'
-
 class User
-  attr_accessor :source_id, :source, :empl_id, :webct_id, :full_name, :last_name,
-                :first_name, :name_prefix, :name_suffix, :nickname, :email,
-                :datasource, :is_mail_forwarded, :status, :role, :exists
-
-  attr_accessor :errors
-#TODO: Change the BBLEARN values below to dbTable (after they are global)
-  def initialize(opts = {})
-    @errors = ActiveRecord::Errors.new(self)
-  end
-
-  def save
-  end
-
-
-  def save!
-  end
-
-  def new_record?
-    false
-  end
-
-
-  def update_attribute
-  end
-
-
-  include ActiveRecord::Validations
+  attr_accessor :id, :first_name, :last_name, :email
 
   def self.find_by_course(course_id)
     sql = <<__SQL__
     SELECT usr.USER_ID, usr.FIRSTNAME, usr.LASTNAME, usr.EMAIL
-    FROM BBLEARN.USERS usr
+    FROM dbTable.USERS usr
     JOIN (SELECT csu.USERS_PK1
-          FROM BBLEARN.COURSE_USERS csu
+          FROM dbTable.COURSE_USERS csu
           JOIN (SELECT csm.PK1
-                FROM BBLEARN.COURSE_MAIN csm
+                FROM dbTable.COURSE_MAIN csm
                 WHERE csm.COURSE_ID = :course_id) csi
           ON csu.CRSMAIN_PK1 = csi.PK1) crs
     ON usr.PK1 = crs.USERS_PK1
 __SQL__
 
     # Substitute values into query and execute:
-#    sql = sql.gsub!("dbTable",$bbl_db_table)
-    $bbl_db_conn = OCI8.new(AppConfig.bbl_db_user, AppConfig.bbl_db_password, AppConfig.bbl_db_string)
-    cursor = $bbl_db_conn.parse(sql)
+    cursor = User.build_cursor(sql)
     cursor.bind_param(':course_id', course_id)
-    cursor.exec
 
-    # Load results into array, log and return:
-    users = Array.new
-    while rs_row = cursor.fetch do
-      users.push(rs_row.to_s)
-    end
 #    logger.info ' *** The course \''+course_id+'\' found the following users: '+users.join(",")
-    return cursor
+    return User.build_user(cursor)
   end
 
-
-  def find_by_course_and_role(course_id,role_name)
+  def self.find_by_course_and_role(course_id,role_name)
     sql = <<__SQL__
     SELECT usr.USER_ID, usr.FIRSTNAME, usr.LASTNAME, usr.EMAIL
-    FROM BBLEARN.USERS usr
+    FROM dbTable.USERS usr
     JOIN (SELECT csu.USERS_PK1
-          FROM BBLEARN.COURSE_USERS csu
+          FROM dbTable.COURSE_USERS csu
           JOIN (SELECT csm.PK1
-                FROM BBLEARN.COURSE_MAIN csm
+                FROM dbTable.COURSE_MAIN csm
                 WHERE csm.COURSE_ID = :course_id) csi
           ON csu.CRSMAIN_PK1 = csi.PK1
           WHERE csu.ROLE = :role_name) crs
@@ -74,20 +37,42 @@ __SQL__
 __SQL__
 
     # Substitute values into query and execute:
-    sql = sql.gsub!("dbTable",$bbl_db_table)
-    cursor = $bbl_db_conn.parse(sql)
+    cursor = User.build_cursor(sql)
     cursor.bind_param(':course_id', course_id)
     cursor.bind_param(':role_name', role_name)
-    cursor.exec
 
-    # Load results into array, log and return:
-    users = Array.new
-    while rs_row = cursor.fetch do
-      users.push(rs_row.to_s)
-    end
-    logger.info ' *** The course \''+course_id+'\' found the following users with role \''+role_name+'\': '+users.join(",")
-    return users
+
+    #logger.info ' *** The course \''+course_id+'\' found the following users with role \''+role_name+'\': '+users.join(",")
+    return User.build_user(cursor)
   end
+
+  def self.build_cursor(sql)
+    $bbl_db_table = AppConfig.bbl_db_table
+    sql = sql.gsub!("dbTable",$bbl_db_table)
+    $bbl_db_conn = OCI8.new(AppConfig.bbl_db_user, AppConfig.bbl_db_password, AppConfig.bbl_db_string)
+    cursor = $bbl_db_conn.parse(sql)
+  end
+
+  def self.build_user(cursor)
+    cursor.exec
+    users = Array.new
+
+    while rs_row = cursor.fetch do
+      user            = User.new
+      user.id         = rs_row[0]
+      user.first_name = rs_row[1]
+      user.last_name  = rs_row[2]
+      user.email      = rs_row[3]
+      users.push(user)
+    end
+
+    if users.size > 1
+      return users
+    else
+      return user
+    end
+  end
+
 
 end
 
