1e8757426ec875631515b6b67e87be3d6bf8d822
James Weir <jweir1@mail.csuchico.edu>
2011-08-09 14:29:37 -0700

    Tweaking test/jimmy files

--- blackboard_ws/test.rb
+++ blackboard_ws/test.rb
@@ -1,29 +1,13 @@
 require 'context_ws'
-require 'course_membership_ws'
-require 'course_ws'
-require 'user_ws'
 
 class Main
 
-	con = ContextWS.new
+  foo = ContextWS.new
+  session_id = foo.ws_initialize
 
-	session_id = con.ws_initialize
-
-#	mem = CourseMembershipWS.new(session_id)
-#	crs = CourseWS.new(session_id)
-#	usr = UserWS.new(session_id)
-
-#    con.ws_login_tool
-#    con.ws_login
-#    con.ws_emulate_user
-#    crs.ws_initialize_course
-#    crs.ws_get_course
-#    usr.ws_initialize_user
-#    usr.ws_get_user
-#    mem.ws_initialize_course_membership
-#    con.ws_get_memberships
-#    mem.ws_delete_course_membership
-
+  foo.ws_login_tool
+  foo.ws_emulate_user
 
 end
-
+  
+  
