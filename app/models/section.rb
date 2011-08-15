4e5024cb209c8c98df066fc03eb947b4b3291c3b
Michael Wood <mykewould@gmail.com>
2011-08-15 10:45:21 -0700

    Sections and HelpDesk/Roles in semi-working condition

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,5 +5,40 @@ class Section < ActiveRecord::Base
   has_many :section_roles, :foreign_key => "crsmain_pk1"
   has_many :users, :through => :course_roles, :foreign_key => "crsmain_pk1"
 
+
+
+   def self.find_by_instructor_id(instructor_id)
+      section_roles = SectionRole.find(:all,
+          :conditions =>
+          ['users_pk1 = :pk1 and role = P', {:id => instructor_id}])
+      sections = []
+      section_roles.each do |s|
+          sections << s.section
+      end
+      return sections
+   end 
+
+   def self.create(course_id)
+    con   = ContextWS.new
+    token = con.ws
+    con.login_tool
+    con.emulate_user
+    sec   = SectionWS.new(token)
+    sec.ws
+    sec.create_course :course_id => course_id,
+                      :name => course_id
+   end
+
+   def self.destroy(pk1)
+    con   = ContextWS.new
+    token = con.ws
+    con.login_tool
+    con.emulate_user
+    sec   = SectionWS.new(token)
+    sec.ws
+    sec.delete_course :pk1 => pk1
+
+   end
+
 end
 
