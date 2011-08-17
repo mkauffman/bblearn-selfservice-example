d36f06255853075a71adc8bad014832091ec0dca
James Weir <jweir1@mail.csuchico.edu>
2011-08-17 14:47:44 -0700

    Removed staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,4 +5,4 @@ role :web, "apphostdev.csuchico.edu"
 role :app, "apphostdev.csuchico.edu"
 role :db,  "apphostdev.csuchico.edu", :primary=>true
 set :branch, "master"
-set :repository_cache, "git_staging"
+set :repository_cache, "git_master"
