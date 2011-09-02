00838d743a26e1a1503b2588f6fc1ca468197b4e
Michael Wood <mykewould@gmail.com>
2011-09-02 10:59:44 -0700

    Merging with Jimmy's file

--- app/controllers/migration_controller.rb
+++ app/controllers/migration_controller.rb
@@ -18,29 +18,29 @@ class MigrationController < ApplicationController
     end
   end
 
-  def exec
-    if params[:sections].nil?
-      flash[:error] = "No section was checked.  Please select a section to migrate first."
-      redirect_to :action => "index" and return
-    end
-    sql = "INSERT INTO bbl_migration (datestamp, sourcedid, instructorid, status, last, first, email, section_description) VALUES "
-    sql += "(:timestamp, :sourcedid, :instructorid, :status, :lastname, :firstname, :email, :section_description)"
-    cursor = $vista_db_conn.parse(sql)
-    for section_id in params[:sections]
-      section = Section.find_section_by_section_id(section_id).first
-      user = Person.find(session[:on_behalf_of]).first
-      cursor.bind_param(':timestamp', Time.now.strftime("%Y%m%d%H%M%S"))
-      cursor.bind_param(':sourcedid', section.section_id)
-      cursor.bind_param(':instructorid', session[:on_behalf_of])
-      cursor.bind_param(':status', "pending")
-      cursor.bind_param(':lastname', user.last_name)
-      cursor.bind_param(':firstname', user.first_name)
-      cursor.bind_param(':email', user.email)
-      cursor.bind_param(':section_description', section.short_name)
-      response = cursor.exec
-      $vista_db_conn.commit
-    end
-    redirect_to :action => "index" and return
+
+  def add
+
+    @migration  = Migration.build
+    @section    = Section.find()
+    @user       = User.find()
+
+    @migration.save!
+
+    redirect_to :action => 'index'
+
+  end
+
+private
+
+  def add_migration_value
+    @migration.datestamp            = Time.now.strftime("%Y%m%d%H%M%S")
+    @migration.sourceid             = @section.course_id
+    @migration.status               = 'pending'
+    @migration.lastname             = @user.last_name
+    @migration.firstname            = @user.first_name
+    @migration.email                = @user.email
+    @migration.section_description  = @section.short_name 
   end
 
 end
