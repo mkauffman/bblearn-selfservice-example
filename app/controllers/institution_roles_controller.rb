8bdf9118859a83d6df23207be3ba4a8b4c1ec225
Mark Kauffman <mkauffman@csuchico.edu>
2012-04-24 14:35:55 -0700

    can now list a user's institution roles

--- app/controllers/institution_roles_controller.rb
+++ app/controllers/institution_roles_controller.rb
@@ -8,26 +8,26 @@ class InstitutionRolesController < ApplicationController
   end
 
   def user_index
-    @user   = User.find_by_user_id(params[:on_behalf_of])
+    @user   = User.find_by_user_id(params[:user_id])
   end
 
   def edit
-    @institution_role = InstitutionRole.find_by_user_id()
+    # @institution_role = InstitutionRole.find_by_user_id()
   end
 
   def new
-    @institution_role.create
+    # @institution_role.create
   end
 
   def update
-    @institution_role = InstitutionRole.find_by_user_id()
-    @institution_role = InstitutionRole.update(params)
+    # @institution_role = InstitutionRole.find_by_user_id()
+    # @institution_role = InstitutionRole.update(params)
   end
 
   def delete
-    logger.info 'Delete self service user, user: '+session[:user]+', delete user: '+params[:portal_id]
-    @institution_role = InstitutionRole.find(:first, :conditions => "portal_id = '"+params[:portal_id]+"'")
-    @institution_role.destroy
-    redirect_to(:action=>"index")
+    # logger.info 'Delete self service user, user: '+session[:user]+', delete user: '+params[:portal_id]
+    # @institution_role = InstitutionRole.find(:first, :conditions => "portal_id = '"+params[:portal_id]+"'")
+    # @institution_role.destroy
+    # redirect_to(:action=>"index")
   end
 end
